1093fe6650b22906963f7f8c1197b3cd7fa81e29
eycyen <ahmthsnpriv@gmail.com>
2026-05-10 14:24:36 +0300

    feat(model): implement baseline linear regression pipeline
    - Split dataset into training and testing sets
    - Apply StandardScaler for feature scaling
    - Train LinearRegression model and evaluate with MSE and R2

---
+++ src/model_training.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv('data/finalscore_cleaned.csv')
+
+X,y = df.drop(columns=["Final_Score"]), df["Final_Score"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = LinearRegression()
+model.fit(X_train, y_train) 
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared: {r2}")
